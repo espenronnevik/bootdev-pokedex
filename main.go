aa008c9368d6c48dab650d5b4100c9adf94f0f9c
Espen Rønnevik <espen@digitalsoup.eu>
2025-07-24 11:04:19 +0200

    Implement basic REPL in main()

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		if err := scanner.Err(); err != nil {
+			fmt.Errorf("Error during input scanning: %w", err)
+			break
+		}
+		inputText := cleanInput(scanner.Text())
+		fmt.Printf("Your command was: %s\n", inputText[0])
+	}
 }
 
 // Split string into words, make lowercase and trim whitespace
 func cleanInput(text string) []string {
-	res := []string{}
-	return res
+	return strings.Fields(strings.ToLower(text))
 }
