9f8d24a4ff83c3731e95c7e6f5d73bcbdeeb04e2
Espen Rønnevik <espen@digitalsoup.eu>
2025-07-25 09:20:30 +0200

    Register two new commands. Create and pass a pointer of the config structure to commands.

--- main.go
+++ main.go
@@ -7,92 +7,48 @@ import (
 	"strings"
 )
 
-type cliCommand struct {
-	name        string
-	description string
-	callback    func() error
-}
-
-var replCommands map[string]cliCommand
-
-func registerReplCommand(name, desc string, cb func() error) error {
-	_, exists := replCommands[name]
-	if exists {
-		return fmt.Errorf("Command already exists")
-	}
-
-	replCommands[name] = cliCommand{
-		name:        name,
-		description: desc,
-		callback:    cb,
-	}
-	return nil
-}
-
-func commandExit() error {
-	fmt.Println("Closing the Pokedex... Goodbye!")
-	os.Exit(0)
-	return nil
-}
-
-func commandHelp() error {
-	fmt.Print("Welcome to the Pokedex!\nUsage:\n\n")
-	for _, v := range replCommands {
-		fmt.Printf("%s: %s\n", v.name, v.description)
-	}
-	return nil
-}
-
-func processCommand(input []string) error {
-	name := input[0]
-	command, ok := replCommands[name]
-	if !ok {
-		return fmt.Errorf("Unknown command: %s", name)
-	}
-
-	err := command.callback()
-	if err != nil {
-		return fmt.Errorf("Error executing command: %s\n", command.name)
-	}
-	return nil
-}
-
 func main() {
-	var err error
+	conf := config{}
 	replCommands = make(map[string]cliCommand)
 
-	err = registerReplCommand("exit", "Exit  the Pokedex", commandExit)
-	if err != nil {
+	if err := registerReplCommand("exit", "Exit  the Pokedex", commandExit); err != nil {
 		fmt.Println("Error registering 'exit' command.")
 	}
 
-	err = registerReplCommand("help", "Display a help message", commandHelp)
-	if err != nil {
+	if err := registerReplCommand("help", "Display a help message", commandHelp); err != nil {
 		fmt.Println("Error registering 'help' command.")
+	}
+
+	if err := registerReplCommand("map", "Display the next batch of location areas", commandMap); err != nil {
+		fmt.Println("Error registering 'map' command.")
+	}
+
+	if err := registerReplCommand("mapb", "Display the previous batch of location areas", commandMapb); err != nil {
+		fmt.Println("Error registering 'mapb' command.")
 	}
 
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
-		ok := scanner.Scan()
+		if ok := scanner.Scan(); !ok {
+			// Scanner reported problems, break out of the loop
+			break
+		}
 
-		if ok {
-			inputText := cleanInput(scanner.Text())
-			if len(inputText) > 0 {
-				err = processCommand(inputText)
-				if err != nil {
-					fmt.Printf("Error: %s\n", err)
-				}
+		inputText := cleanInput(scanner.Text())
+		if len(inputText) > 0 {
+			if err := processCommand(inputText, &conf); err != nil {
+				fmt.Printf("Error: %s\n", err)
 			}
-		} else {
-			if err = scanner.Err(); err != nil {
-				fmt.Printf("Error occured during input scanning: %s\n", err)
-				os.Exit(1)
-			}
-			// EOF on stdin, call exit command
-			commandExit()
 		}
 	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("Error occured during input scanning: %s\n", err)
+		os.Exit(1)
+	}
+	// EOF on stdin, call exit command
+	commandExit(nil)
 }
 
 // Split string into words, make lowercase and trim whitespace
